b5d66ddfa28fe426fa711a32a5f0d9797cc4075f
Stareat0707 <stareat0707@gmail.com>
2018-10-25 10:14:32 +0900

    C++ Variadics

---
+++ C++ Variadics/Main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+template <bool digit>
+int reversed_binary_value()
+{
+	return static_cast<int>(digit);
+}
+
+template <bool first, bool second, bool...less>
+int reversed_binary_value()
+{
+	return (reversed_binary_value<second, less...>() << 1) + static_cast<int>(first);
+}
+
+template <int n, bool...digits>
+struct CheckValues
+{
+	static void check(int x, int y)
+	{
+		CheckValues<n - 1, 0, digits...>::check(x, y);
+		CheckValues<n - 1, 1, digits...>::check(x, y);
+	}
+};
+
+template <bool...digits>
+struct CheckValues<0, digits...>
+{
+	static void check(int x, int y)
+	{
+		int z = reversed_binary_value<digits...>();
+		std::cout << (z + 64 * y == x);
+	}
+};
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; ++i)
+	{
+		int x, y;
+		cin >> x >> y;
+
+		CheckValues<6>::check(x, y);
+		cout << endl;
+	}
+}
